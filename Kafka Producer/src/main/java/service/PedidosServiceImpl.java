e41172d95ba92b1ea7240ee758789facb9b5b33a
JULIAN GOMEZ FERNANDEZ <22173008+jgf78@users.noreply.github.com>
2024-10-10 16:02:13 +0200

    Add files via upload

---
+++ Kafka Producer/src/main/java/service/PedidosServiceImpl.java
@@ -0,0 +1,35 @@
+package service;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import model.Pedido;
+@Service
+public class PedidosServiceImpl implements PedidosService {
+	
+	@Value("${topic}")
+	String topico;
+	
+	@Autowired
+	KafkaTemplate<String, Pedido> kafkaTemplate;
+
+	@Override
+	public void registrarPedido(Pedido pedido) {
+		CompletableFuture<SendResult<String,Pedido>> future=kafkaTemplate.send(topico, pedido);
+		future.whenCompleteAsync((r, t) -> {
+		    if (t != null) {
+		        t.printStackTrace();
+		        throw new RuntimeException("Error al enviar mensaje a Kafka", t);
+		    }
+		    System.out.println("Se ha registrado el pedido " + r.getProducerRecord().value().getNombre() +
+		                       " en el tópico " + r.getRecordMetadata().topic());
+		});
+
+	}
+
+}
